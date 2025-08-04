e8a31b4112b41f4b5555757c36c2617f8cb39398
J SURYA KIRAN <kiransurya93@kluniversity.in>
2025-08-04 10:01:08 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'jenkinserp', // deployment folder name but not git repo name
 })
